de9fa686a407b2afedb421a1e5f97f40ae022f1b
Masudur Rahman <masud@appscode.com>
2022-05-27 18:11:55 +0600

    Use separate api domains for api, accounts, and base server urls
    Signed-off-by: Masudur Rahman <masud@appscode.com>

--- bytebuilders.go
+++ bytebuilders.go
@@ -5,6 +5,16 @@ import (
 	"strings"
 )
 
+func GetBaseDomain(host string) string {
+	addr, err := SplitHostPortDefault(host, "", "")
+	if err != nil {
+		return "byte.builders"
+	}
+	parts := strings.Split(addr.Host, ".")
+	base := strings.Join(parts[1:], ".")
+	return base
+}
+
 func GetBaseUrl(host string, production bool) string {
 	var baseUrl string
 	baseDomain := GetBaseDomain(host)
@@ -16,12 +26,24 @@ func GetBaseUrl(host string, production bool) string {
 	return baseUrl
 }
 
-func GetBaseDomain(host string) string {
-	addr, err := SplitHostPortDefault(host, "", "")
-	if err != nil {
-		return "byte.builders"
+func GetApiUrl(host string, production bool) string {
+	var apiUrl string
+	baseDomain := GetBaseDomain(host)
+	if production {
+		apiUrl = fmt.Sprintf("https://api.%v", baseDomain)
+	} else {
+		apiUrl = fmt.Sprintf("http://api.%v:3003", baseDomain)
 	}
-	parts := strings.Split(addr.Host, ".")
-	base := strings.Join(parts[1:], ".")
-	return base
+	return apiUrl
+}
+
+func GetAccountsUrl(host string, production bool) string {
+	var accountsUrl string
+	baseDomain := GetBaseDomain(host)
+	if production {
+		accountsUrl = fmt.Sprintf("https://accounts.%v", baseDomain)
+	} else {
+		accountsUrl = fmt.Sprintf("http://accounts.%v:3000", baseDomain)
+	}
+	return accountsUrl
 }
